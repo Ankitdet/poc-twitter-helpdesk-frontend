39e20d13606e2ebd01a63aab27dd1d8706528791
ankit <ankitdetroja@gmail.com>
2020-12-24 11:45:10 +0530

    cioomit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 // API HOST URLs
 let HOST_URL;
-if (window.location.origin === "https://twitter--helpdesk.herokuapp.com") {
-  HOST_URL = "https://twitter-helpdesk--server.herokuapp.com";
+if (window.location.origin === "https://poc-twitter-helpdesk-frontend.herokuapp.com/") {
+  HOST_URL = "https://poc-twitter-helpdesk-backend.herokuapp.com/";
 } else {
   HOST_URL = "http://localhost:5000";
   // HOST_URL = "https://twitter-helpdesk--server.herokuapp.com";
